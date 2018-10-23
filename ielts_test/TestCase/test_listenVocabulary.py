c4d8dd1d297f1677a397e448d7013caea8405aa5
allen <540267533@qq.com>
2018-10-23 23:32:13 +0800

    更改听力词汇测试类
    更改听力词汇测试类

--- ielts_test/TestCase/test_listenVocabulary.py
+++ ielts_test/TestCase/test_listenVocabulary.py
@@ -6,7 +6,7 @@ from TestCase.basic_unittest import *
 import unittest
 from selenium.webdriver.common.by import By
 
-class LoginTest(basic_unittest):
+class ListenVocTest(basic_unittest):
     def setUp(self):
         self.driver.launch_app()
 
@@ -22,9 +22,9 @@ class LoginTest(basic_unittest):
     def tearDown(self):
         self.driver.close_app()
 
-# if __name__ =="__main__":
-#     testsuit = unittest.TestSuite()
-#     testsuit.addTest(LoginTest("test_login"))
-#     testsuit.addTest(LoginTest("test_login2"))
+if __name__ =="__main__":
+    testsuit = unittest.TestSuite()
+    testsuit.addTest(ListenVocTest("test_ListenVoc"))
+    # testsuit.addTest(ListenVocTest("test_login2"))
 
 
